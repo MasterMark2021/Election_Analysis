1cb599e6958b0620df6a279db72d9c1f785fbb16
Mark O <markokoli85@gmail.com>
2021-11-16 14:20:16 -0500

    updated

--- PyPoll.py
+++ PyPoll.py
@@ -1,3 +1,4 @@
+
 # Add our dependencies.
 import csv
 # Assign a variable to load a file from a path.
@@ -11,6 +12,9 @@ total_votes = 0
 # Candidate Options
 candidate_options = []
 
+# 1. Declare the empty dictionary.
+candidate_votes = {}
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -18,6 +22,8 @@ with open(file_to_load) as election_data:
 
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
+    # Read the header row.
+    headers = next(file_reader)
 
     #Print each row in the CSV file.
     for row in file_reader:
@@ -31,5 +37,10 @@ if candidate_name not in candidate_options :
 # Add the candidate name to the candidate list
        candidate_options.append(candidate_name)
 
-# Print the candidate list.
-print(candidate_options)
+   # 2. Begin tracking that candidate's vote count.
+candidate_votes[candidate_name] = 0
+# Add a vote to that candidate's count.
+candidate_votes[candidate_name]  += 1
+
+        # Print the candidate vote dictionary.
+print(candidate_votes)
