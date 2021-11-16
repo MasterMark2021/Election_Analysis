b32ca86deab558e2938e0b6e4b90c9d086355408
Mark O <markokoli85@gmail.com>
2021-11-16 14:49:36 -0500

    updated

--- PyPoll.py
+++ PyPoll.py
@@ -34,6 +34,7 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
         # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
+        
 
 for candidate_name in candidate_votes:
     # Retrieve vote count and percentage.
@@ -48,12 +49,16 @@ for candidate_name in candidate_votes:
         winning_count = votes
         winning_candidate = candidate_name
         winning_percentage = vote_percentage
-# Print the winning candidates' results to the terminal.
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
 
-print(winning_candidate_summary)
+# Save the results to our text file.
+with open(file_to_save, "w") as txt_file:
+
+# Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
